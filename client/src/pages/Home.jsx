433d4cd8af0e13842e77e0574b5f5e4e7f1b0a43
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-04-04 16:44:15 +0200

    update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState} from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { useSnapshot } from 'valtio'
+import { CustomButton } from '../components';
 
 import { headContainerAnimation, 
     headContentAnimation, 
@@ -11,8 +12,39 @@ import state from '../store';
 
 const Home = () => {
   const snap = useSnapshot(state);
+
   return (
-    <div>Home</div>
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className='home' {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img src='./threejs.png' alt="logo" className='w-8 h-8 object-contain' />
+          </motion.header>
+
+          <motion.div className='home-content' {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className='head-text'>
+                LET'S <br className='xl:block hidden'/> DO IT.
+              </h1>
+            </motion.div>
+
+            <motion.div {...headContentAnimation} className='flex flex-col gap-5'>
+              <p className='max-w-md font-normal text-gray-600 text-base'> Create your unique and exclusive shirt with our brand new 3D
+                customization toll. <strong>Unleash your imagination</strong>{" "}
+                and <em>define your fantastic style</em>.
+              </p>
+
+              <CustomButton 
+                type="filler"
+                title="Customize It"
+                handleClick={()=> state.intro = false}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                />
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
   )
 }
 
