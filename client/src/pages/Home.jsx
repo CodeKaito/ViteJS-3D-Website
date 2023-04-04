723cf489af9fb40b6c3cad73d5881ae9453ea7fc
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-04-04 16:18:51 +0200

    update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState} from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+import { useSnapshot } from 'valtio'
+
+import { headContainerAnimation, 
+    headContentAnimation, 
+    headTextAnimation, 
+    slideAnimation } from '../config/motion';
+
+import state from '../store';
 
 const Home = () => {
+  const snap = useSnapshot(state);
   return (
     <div>Home</div>
   )
