037d43b220215e7f2fac3eca840c6ef9f6f8a273
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-04-05 11:24:04 +0200

    custom

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -1,8 +1,9 @@
-import React, {useRef} from 'react'
-import { useFrame } from '@react-three/fiber'
-import { easing } from 'maath'
-import { useSnapshot } from 'valtio'
-import state from '../store'
+import React, { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
 
 const CameraRig = ({ children }) => {
   const group = useRef();
@@ -12,14 +13,14 @@ const CameraRig = ({ children }) => {
     const isBreakpoint = window.innerWidth <= 1260;
     const isMobile = window.innerWidth <= 600;
 
-    // set initial position of the model
+    // set the initial position of the model
     let targetPosition = [-0.4, 0, 2];
     if(snap.intro) {
-      if(isBreakpoint) targetPosition = [0,0,2];
+      if(isBreakpoint) targetPosition = [0, 0, 2];
       if(isMobile) targetPosition = [0, 0.2, 2.5];
     } else {
-      if(isMobile) targetPosition = [0,0,2.5]
-      else targetPosition = [0,0,2]
+      if(isMobile) targetPosition = [0, 0, 2.5]
+      else targetPosition = [0, 0, 2];
     }
 
     // set model camera position
@@ -28,13 +29,12 @@ const CameraRig = ({ children }) => {
     // set the model rotation smoothly
     easing.dampE(
       group.current.rotation,
-      [state.pointer.y / 10, -state.pointer.x / 5,0 ],
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
       0.25,
       delta
     )
   })
 
-  
 
   return <group ref={group}>{children}</group>
 }
